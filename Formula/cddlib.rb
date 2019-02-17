d637197873cd2568fda6efe22c561fc59ccdb776
Gabriele Balletti <gabrieleballetti@gmail.com>
2019-02-17 20:23:03 +0100

    Added cddlib

---
+++ Formula/cddlib.rb
@@ -0,0 +1,21 @@
+class Cddlib < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/cddlib/cddlib/releases/download/0.94j/cddlib-0.94j.tar.gz"
+  sha256 "27d7fcac2710755a01ef5381010140fc57c95f959c3c5705c58539d8c4d17bfb"
+  
+  depends_on "gmp"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"main.c").write("int main () {}")
+    system "gcc", "-I/usr/local/include", "-L/usr/local/lib", "main.c", "-lcdd"
+  end
+end
