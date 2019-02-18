14f7f89258a71d924c0bd1307d2ac992fabf7966
Gabriele Balletti <38753055+gabrieleballetti@users.noreply.github.com>
2019-02-18 23:18:17 +0100

    Update cddlib.rb

--- Formula/cddlib.rb
+++ Formula/cddlib.rb
@@ -1,6 +1,6 @@
 class Cddlib < Formula
-  desc ""
-  homepage ""
+  desc "A C implementation of the Double Description Method of Motzkin et al."
+  homepage "https://www.inf.ethz.ch/personal/fukudak/cdd_home/"
   url "https://github.com/cddlib/cddlib/releases/download/0.94j/cddlib-0.94j.tar.gz"
   sha256 "27d7fcac2710755a01ef5381010140fc57c95f959c3c5705c58539d8c4d17bfb"
   
